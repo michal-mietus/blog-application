049ff11172ae0799e1cbc988e344ebc787e3864b
goodude123 <assasinmcpl@gmail.com>
2018-11-27 01:06:42 +0100

    Login screen. User here retrieves auth token from api.

---
+++ screens/Login.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import { View, Button, StyleSheet, Text, AsyncStorage } from 'react-native';
+import { NavigationActions, StackActions } from 'react-navigation'
+import t from 'tcomb-form-native'; // 0.6.9
+import Spinner from 'react-native-loading-spinner-overlay';
+
+
+const Form = t.form.Form;
+
+const User = t.struct({
+  username: t.String,
+  password: t.String,
+});
+
+
+export default class LoginScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loginInfo: ''}
+  }
+
+
+  handleSubmit = () => {
+    this.setState({spinner: true})  // turn on spinner
+    const value = this._form.getValue();
+    const url = "http://hostingsme.pythonanywhere.com/api/api-token-auth/";
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(value),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .then(
+      response => this.isTokenReceived(response)
+    )
+    .catch(error => console.error('Error:', error)); 
+  }
+
+  isTokenReceived(response){
+    this.setState({spinner: false})  // turn off spinner 
+    console.log('Success:', JSON.stringify(response));
+    if ("token" in response){
+      this.setState({loginInfo: 'Logged!'});  // just info
+      this.saveTokenToAsyncStorage(response["token"]);
+      this.resetStackAndShowHome();
+    } else {
+      this.setState({loginInfo: 'Invalid logging'});
+    }
+  }
+
+  resetStackAndShowHome = () => {
+    // forbids going back to login screen
+    this.props
+      .navigation
+      .dispatch(StackActions.reset({
+        index: 0,
+        actions: [
+          NavigationActions.navigate({
+            routeName: 'Home',
+          }),
+        ],
+      }))
+   }
+
+  saveTokenToAsyncStorage(token){
+    console.log('Saving token:', String(token));
+    const TOKEN = 'token';
+    AsyncStorage.setItem(TOKEN, token);
+  }
+
+  componentDidMount() {
+      this.setState({spinner: false});
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Spinner
+          visible={this.state.spinner}
+          textContent={'Loading...'}
+          textStyle={styles.spinnerTextStyle}
+        />
+        <Form 
+          type={User}
+          ref={(c) => this._form = c}
+        />
+        <Button
+          title="Login"
+          onPress={this.handleSubmit}
+        />
+        <Text style={styles.information}>{this.state.loginInfo}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    spinnerTextStyle: {
+      color: '#FFF'
+    },
+    container: {
+      justifyContent: 'center',
+      marginTop: 50,
+      padding: 20,
+      backgroundColor: '#ffffff',
+    },
+    information: {
+      padding: 30,
+      textAlign: 'center',
+      fontSize: 16,
+      color: 'black',
+      fontWeight: 'bold'
+    },
+  });
+  

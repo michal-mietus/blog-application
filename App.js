f7e615fc940716db1d8a0091790745c9f89c3b3e
goodude123 <assasinmcpl@gmail.com>
2018-11-27 21:28:45 +0100

    Added ArticlesScreen to navigator.

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import HomeScreen from './screens/Home'
 import LoginScreen from './screens/Login'
+import ArticlesScreen from './screens/Articles'
 
 
 const AppNavigator = createStackNavigator(
   {
     Login: LoginScreen,
     Home: HomeScreen,
+    Articles: ArticlesScreen,
   },
   {
     initialRouteName: "Login",
